1553280bacc78fbff1e3051192bc373e04efed7b
agam-sb <agam@superbeings.ai>
2022-06-09 14:08:13 +0530

    changed output format from List<Record> to Map<String,Record>

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+
 import utils.Commons;
 import pojos.FieldType;
 import pojos.Record;
@@ -13,13 +12,13 @@ public class Main {
     public static final String COMMA_STR = ",";
     public static final String OUTPUT_PATH = "C:\\Users\\agams\\OneDrive\\Desktop\\coda-assignment\\output";
 
-    private static List<List<Record>> processData(String[] headers, List<String[]> userDataList) {
-        List<List<Record>> employeeRecords = new ArrayList<>();
+    private static List<Map<String, Record>> processData(String[] headers, List<String[]> userDataList) {
+        List<Map<String, Record>> employeeRecords = new ArrayList<>();
         int expectedFieldTypesCount = headers.length;
         int totalRecords = userDataList.size();
 
         for(int i = 0; i < totalRecords; i++){
-            List<Record> employeeRecord = new ArrayList<>();
+            Map<String, Record> employeeRecord = new HashMap<>();
             String[] userData = userDataList.get(i);
 
             if(userData.length != expectedFieldTypesCount){
@@ -29,7 +28,7 @@ public class Main {
 
             for(int j = 0; j < expectedFieldTypesCount; j++){
                 FieldType type = RegexValidator.getFieldType(userData[j]);
-                employeeRecord.add(new Record(headers[j].trim(), userData[j].trim(), type.getText()));
+                employeeRecord.put(headers[j].trim(), new Record(userData[j].trim(), type.getText()));
             }
 
             employeeRecords.add(employeeRecord);
@@ -37,7 +36,7 @@ public class Main {
         return employeeRecords;
     }
 
-    private static void convertToJsonAndSave(List<List<Record>> employeeRecords) {
+    private static void convertToJsonAndSave(List<Map<String, Record>> employeeRecords) {
         for(int i = 0; i < employeeRecords.size(); i++){
             try {
                 FileWriter writer = new FileWriter(new File(OUTPUT_PATH, "user"+ (i+1) +".json"));
@@ -78,7 +77,7 @@ public class Main {
             userDataList.add(fileScanner.nextLine().split(COMMA_STR));
         }
 
-        List<List<Record>> employeeRecords = processData(headers, userDataList);
+        List<Map<String, Record>> employeeRecords = processData(headers, userDataList);
 
         convertToJsonAndSave(employeeRecords);
     }
--- src/pojos/Record.java
+++ src/pojos/Record.java
@@ -1,12 +1,10 @@
 package pojos;
 
 public class Record {
-    String field;
     String value;
     String type;
 
-    public Record(String field, String value, String type) {
-        this.field = field;
+    public Record(String value, String type) {
         this.value = value;
         this.type = type;
     }
